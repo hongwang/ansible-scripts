d5063b31467bc2f81a81a2209426466a855acede
Hong Wang <hongwang@hcdlearning.com>
2017-04-27 14:59:13 +0800

    add more zeppelin settings

--- roles/spark/templates/opt/spark/conf/spark-env.sh
+++ roles/spark/templates/opt/spark/conf/spark-env.sh
@@ -1,1 +1,3 @@
+SPARK_MASTER_OPTS="-Dspark.deploy.defaultCores=1"
+SPARK_WORKER_OPTS="-Dspark.worker.cleanup.enabled=true -Dspark.worker.cleanup.interval=7200"
 SPARK_DAEMON_JAVA_OPTS="-Dspark.deploy.recoveryMode=ZOOKEEPER -Dspark.deploy.zookeeper.url=uat-zk-01:2181,uat-zk-02:2181,uat-zk-03:2181 -Dspark.deploy.zookeeper.dir=/spark"
