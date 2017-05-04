9e40c68605f79a64f2aaf4e051e6354ce807b414
Hong Wang <hongwang@hcdlearning.com>
2017-05-04 14:38:39 +0800

    install py4j for zeppelin

--- roles/zeppelin/templates/opt/zeppelin/conf/zeppelin-env.sh
+++ roles/zeppelin/templates/opt/zeppelin/conf/zeppelin-env.sh
@@ -1,4 +1,4 @@
 export ZEPPELIN_PORT=30001
 
 export SPARK_HOME=/opt/spark
-export SPARK_SUBMIT_OPTIONS="--driver-cores 1 --driver-memory 1G --total-executor-cores 2 --files ${SPARK_HOME}/conf/hive-site.xml"
+export SPARK_SUBMIT_OPTIONS="--driver-memory 1G --total-executor-cores 2 --files ${SPARK_HOME}/conf/hive-site.xml"
