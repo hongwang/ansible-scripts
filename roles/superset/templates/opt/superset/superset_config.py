06f1ff0e84465933c62f7acbc4d4e1381497e22a
Hong Wang <hongwang@hcdlearning.com>
2017-05-19 14:48:06 +0800

    finish superset script

--- roles/superset/templates/opt/superset/superset_config.py
+++ roles/superset/templates/opt/superset/superset_config.py
@@ -5,6 +5,7 @@ ROW_LIMIT = 5000
 SUPERSET_WORKERS = 4
 
 SUPERSET_WEBSERVER_PORT = 30002
+SUPERSET_WEBSERVER_TIMEOUT = 600
 #---------------------------------------------------------
 
 #---------------------------------------------------------
@@ -18,10 +19,17 @@ SECRET_KEY = 'sdjklasdjkalsjdfasdfasdfasdfasdf'
 # superset metadata (slices, connections, tables, dashboards, ...).
 # Note that the connection information to connect to the datasources
 # you want to explore are managed directly in the web UI
-SQLALCHEMY_DATABASE_URI = 'mysql://hive:HCDhcd123@sh-mysql-01:3306/superset'
+SQLALCHEMY_DATABASE_URI = 'mysql://hive:HCDhcd123@uat-mysql-01:3306/superset'
 
 # Flask-WTF flag for CSRF
-CSRF_ENABLED = True
+WTF_CSRF_ENABLED = True
 
 # Set this API key to enable Mapbox visualizations
 MAPBOX_API_KEY = ''
+
+
+CACHE_CONFIG = {'CACHE_TYPE': 'simple'}
+TABLE_NAMES_CACHE_CONFIG = {'CACHE_TYPE': 'simple'}
+
+
+SQLLAB_TIMEOUT = 600
